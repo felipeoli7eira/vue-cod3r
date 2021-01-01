a5d02aa33ab19ddfbfeb22c8a777f6b757acfc34
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-01-01 16:37:44 -0300

    http with axios

--- sec-15-http/src/plugins/axios.js
+++ sec-15-http/src/plugins/axios.js
@@ -2,13 +2,28 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://curso-vue-60176.firebaseio.com/'
+// axios.defaults.baseURL = 'https://curso-vue-60176.firebaseio.com/'
+
+/** setando headers padrão em todas as requisições (Global) */
+// axios.defaults.headers.commom['Authorization'] = '...token...'
+// axios.defaults.headers.get['Accepts'] = 'application/json'
 
 Vue.use(
     {
         install(Vue)
         {
-            Vue.prototype.$http = axios
+            // Vue.prototype.$http = axios
+
+            Vue.prototype.$http = axios.create(
+                {
+                    baseURL: 'https://curso-vue-60176.firebaseio.com/',
+                    headers: {
+                        get: {
+                            'Authorization': '...token...'
+                        }
+                    }
+                }
+            )
 
             // interceptor de requisição
             Vue.prototype.$http.interceptors.request.use(
@@ -23,7 +38,7 @@ Vue.use(
 
             Vue.prototype.$http.interceptors.response.use(
                 response => {
-                    console.log(reponse)
+                    console.log(response)
                 },
                 error => console.log(error)
             )
