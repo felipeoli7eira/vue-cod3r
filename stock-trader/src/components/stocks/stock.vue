92a6f897d7a6829aa6dc3e1e190f26bdb332c8bb
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-01-04 21:30:36 -0300

    stocks grid and buyStock method

--- stock-trader/src/components/stocks/stock.vue
+++ stock-trader/src/components/stocks/stock.vue
@@ -1,11 +1,49 @@
 <template>
-    <h1>action</h1>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+        <v-card class="green darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>{{ stock.name }} <small>(preço: {{ stock.price }})</small></strong>
+            </v-card-title>
+        </v-card>
+
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label="Quantidade" type="number" v-model.number="qtt" />
+                <v-btn @click="buyStock" class="green darken-3 white--text" :disabled="qtt <= 0 || !Number.isInteger(qtt)">comprar</v-btn>
+            </v-container>
+        </v-card>
+    </v-flex>
 </template>
 
 <script>
-export default {
 
-}
+    export default {
+        name: 'Stock',
+        props: ['stock'],
+
+        data()
+        {
+            return {
+                qtt: 0
+            }
+        },
+
+        methods: {
+            buyStock()
+            {
+                let order = {
+                    stock_id: this.stock.id,
+                    stock_price: this.stock.price,
+                    stock_qtt: this.qtt
+                }
+
+                console.log(order)
+
+                this.qtt = 0
+            }
+        }
+    }
+
 </script>
 
 <style>
--- stock-trader/src/components/stocks/stocks.vue
+++ stock-trader/src/components/stocks/stocks.vue
@@ -1,11 +1,51 @@
 <template>
-    <h1>actions</h1>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+    </v-layout>
 </template>
 
 <script>
-export default {
 
-}
+    /* components */
+    import Stock from '@/components/stocks/stock'
+
+    export default {
+        name: 'Stocks',
+        components: { Stock },
+
+        data()
+        {
+            return {
+                stocks: [
+                    {
+                        id: 1,
+                        name: 'BMW',
+                        qtt: 5,
+                        price: 105.50
+                    },
+                    {
+                        id: 2,
+                        name: 'Google',
+                        qtt: 2,
+                        price: 730.21
+                    },
+                    {
+                        id: 3,
+                        name: 'Apple',
+                        qtt: 1,
+                        price: 600
+                    },
+                    {
+                        id: 4,
+                        name: 'Microsoft',
+                        qtt: 4,
+                        price: 300.70
+                    },
+                ]
+            }
+        }
+    }
+
 </script>
 
 <style>
